ee2bb7a14fed097613912a323f27c0e787538852
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-03-05 16:58:08 -0800

    shows friends and add friend

--- server.py
+++ server.py
@@ -9,5 +9,14 @@ def index():
     friends = mysql.fetch('SELECT * FROM friends')
     return render_template('index.html', friends=friends)
 
+@app.route('/friends', methods=['POST'])
+def create():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    hometown = request.form['hometown']
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES ('{}', '{}', '{}', NOW(), NOW())".format(first_name, last_name, hometown)
+    mysql.run_mysql_query(query)
+    return redirect('/')
+
 
 app.run(debug=True)
